e6fcf05d33bb56ba26892806a48307efc6a49b45
prasang007 <prasang00@gmail.com>
2020-10-01 17:44:45 +0530

    first commit

---
+++ e1.m
@@ -0,0 +1,13 @@
+close all;
+clear all;
+a = imread('Test.jpeg');
+i = rgb2gray(a);
+subplot(2,2,1);
+imshow(a); title('Real Image');
+subplot(2,2,2);
+imshow(i); title('Grayscale Image');
+w = im2bw(i);
+subplot(2,2,3);
+imshow(w); title(' Binary Image');
+subplot(2,2,4);
+imhist(i); title(' Histogram ');
